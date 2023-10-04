a500c844fa4b0884d88c8ff487aea6dbba63a7f6
azamat <azamatabdurashitov0@gmail.com>
2023-10-05 00:12:40 +0500

    add change password, done, change base.html

--- account/urls.py
+++ account/urls.py
@@ -3,11 +3,18 @@ from account import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #path('login/', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'),
 
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LoginView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    
+    # 2qism parolni ozgartirish
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    
     
     path('',views.dashboard, name='dashboard'),
 ]
